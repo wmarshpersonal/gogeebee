fa1fa5d7674ab2e71597c0d8674c319ce205b11a
Wayne Marsh <bits@waynemarsh.com>
2024-04-03 15:41:10 -0400

    more accurate sxc, scy

--- ppu/fetch.go
+++ ppu/fetch.go
@@ -11,6 +11,7 @@ type fetch struct {
 	tileNo                 int
 	tileAddr               int
 	tileDataLo, tileDataHi uint8
+	scx, scy               uint8
 }
 
 type fetchStep int
@@ -31,7 +32,10 @@ func (f *fetch) fetchBg(
 	fifo *list.List,
 ) {
 	switch f.step {
-	case fetchTile0, fetchDataLo0, fetchDataHi0:
+	case fetchDataLo0, fetchDataHi0:
+		f.step++
+	case fetchTile0:
+		f.scx, f.scy = registers[SCX], registers[SCY]
 		f.step++
 	case fetchTile1:
 		tileMapAddr := tileMap0Address
@@ -39,8 +43,8 @@ func (f *fetch) fetchBg(
 			tileMapAddr = tileMap1Address
 		}
 		var offset int
-		offset += (f.tileX + int(registers[SCX]/8)) & 0x1F
-		offset += int(32 * (((int(registers[LY]) + int(registers[SCY])) & 0xFF) / 8))
+		offset += (f.tileX + int(f.scx/8)) & 0x1F
+		offset += int(32 * (((int(registers[LY]) + int(f.scy)) & 0xFF) / 8))
 		f.tileNo = int(vram[(tileMapAddr+offset&0x3FF)&0x1FFF])
 		f.step++
 	case fetchDataLo1:
@@ -49,7 +53,7 @@ func (f *fetch) fetchBg(
 			addrMode = base8000
 		}
 		f.tileAddr = translateTileDataAddress(addrMode, f.tileNo)
-		f.tileAddr += 2 * int((registers[LY]+registers[SCY])%8)
+		f.tileAddr += 2 * int((registers[LY]+f.scy)%8)
 		f.tileDataLo = vram[f.tileAddr&0x1FFF]
 		f.step++
 	case fetchDataHi1:
